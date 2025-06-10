5d92cfbe817fc7e0300916a24f43967f67d66ec2
theanhdola <thetruong275@gmail.com>
2025-06-10 17:51:09 +0700

    remove example environment files and update server configuration

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,12 +6,14 @@ import apiRoutes from './api';
 const router = Router();
 
 router.use('/auth', localAuthRoutes);
-router.use('/auth', googleAuthRoutes);
-router.use('/auth', facebookAuthRoutes);
+// router.use('/auth', googleAuthRoutes);
+// router.use('/auth', facebookAuthRoutes);
 router.use('/api', apiRoutes);
 // fallback 404
 router.use('/api', (req, res) => res.status(404).json('No route for this path'));
-
+router.get('/', (req, res) => {
+  res.status(200).send('<h1>Server của bạn đang chạy!</h1><p>Đây là trang chủ.</p>');
+});
 export default router;
 
 /*
